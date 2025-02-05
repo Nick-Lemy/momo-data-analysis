f37d28f2f6947c155d1e7525c9f9d36f133d5a48
Nick-Lemy <n.kayiranga@alustudent.com>
2025-02-06 00:21:10 +0200

    Update server file

--- server.js
+++ server.js
@@ -1,28 +1,105 @@
-import { XMLParser } from "fast-xml-parser";
 import { readFileSync } from "node:fs";
+import { Parser } from "xml2js";
 
-//xml file from https://learn.microsoft.com/en-us/previous-versions/windows/desktop/ms762271(v=vs.85)
 const xmlFile = readFileSync(`${process.cwd()}/modified_sms_v2.xml`, "utf8");
-const parser = new XMLParser();
-const json = parser.parse(xmlFile);
-const smses = json.sms_data.sms;
 
-console.log();
-class Transaction {
-  constructor(type, source, value, envolved) {
-    this.type = type;
-    this.source = source;
-    this.value = value;
-    this.envolved = envolved;
-  }
-}
-for (const sms of smses) {
-  //   console.log(sms.body);
-  const data = sms.body.split(" ");
-  const value = data.filter((n) => Number(n)).map((n) => Number(n));
-  console.log("Value", value, "RWF");
-  //   console.log("Fees:", fee, " RWF");
-  console.log(data);
+export default function extractAttributes(xmlString) {
+  const parser = new Parser({
+    explicitArray: false,
+    attrkey: "attributes",
+  });
+
+  return new Promise((resolve, reject) => {
+    parser.parseString(xmlString, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        let smsElements = result.smses.sms;
+        if (!Array.isArray(smsElements)) {
+          smsElements = [smsElements];
+        }
+
+        const categorizedData = {
+          "Incoming Money": [],
+          "Payments to Code Holders": [],
+          "Transfers to Mobile Numbers": [],
+          "Bank Deposits": [],
+          "Airtime Bill Payments": [],
+          "Cash Power Bill Payments": [],
+          "Transactions Initiated by Third Parties": [],
+          "Withdrawals from Agents": [],
+          "Bank Transfers": [],
+          "Internet and Voice Bundle Purchases": [],
+        };
+
+        smsElements.forEach((sms) => {
+          const body = sms.attributes.body;
+
+          if (/received .* RWF from/i.test(body)) {
+            categorizedData["Incoming Money"].push(sms.attributes);
+          } else if (
+            /Your payment of .* RWF to .* has been completed/i.test(body)
+          ) {
+            categorizedData["Payments to Code Holders"].push(sms.attributes);
+          } else if (/transferred to .* from/i.test(body)) {
+            categorizedData["Transfers to Mobile Numbers"].push(sms.attributes);
+          } else if (
+            /A bank deposit of .* has been added to your mobile money account/i.test(
+              body
+            )
+          ) {
+            categorizedData["Bank Deposits"].push(sms.attributes);
+          } else if (/Your payment of .* RWF to Airtime/i.test(body)) {
+            categorizedData["Airtime Bill Payments"].push(sms.attributes);
+          } else if (/Your payment of .* RWF to MTN Cash Power/i.test(body)) {
+            categorizedData["Cash Power Bill Payments"].push(sms.attributes);
+          } else if (
+            /A transaction of .* by .* on your MOMO account/i.test(body)
+          ) {
+            categorizedData["Transactions Initiated by Third Parties"].push(
+              sms.attributes
+            );
+          } else if (
+            /withdrawn .* RWF from your mobile money account/i.test(body)
+          ) {
+            categorizedData["Withdrawals from Agents"].push(sms.attributes);
+          } else if (/bank transfer/i.test(body)) {
+            categorizedData["Bank Transfers"].push(sms.attributes);
+          } else if (/Kugura ama inite cg interineti/i.test(body)) {
+            categorizedData["Internet and Voice Bundle Purchases"].push(
+              sms.attributes
+            );
+          }
+        });
+
+        resolve(categorizedData);
+      }
+    });
+  });
 }
 
-/* */
+async function main() {
+  let d = await extractAttributes(xmlFile).then((data) => {
+    data = JSON.stringify(data);
+    data = JSON.parse(data);
+  });
+}
+
+main();
+console.log("test");
+
+// extractAttributes(xmlFile)
+//   .then((res) => {
+//     // data = JSON.stringify(res);
+//     return res;
+//   })
+//   .catch(console.error);
+
+// class Transaction {
+//   constructor(type, source, value, envolved) {
+//     this.type = type;
+//     this.source = source;
+//     this.value = value;
+//     this.envolved = envolved;
+//   }
+// }
